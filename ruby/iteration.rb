18aab597f437d3bde71a6658f1fdd34066c50b0d
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-14 14:48:38 -0700

    Created arrays and hash for each and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,35 @@ yield("John Doe")
 end 
 
 good_bye { |name| puts "See ya later! #{name}"}
+
+#Release 1: Use each, map and map!
+# Used .each for array: 
+
+sports = ["football", "soccer", "baseball", "volleyball"]
+
+
+sports.each do |item|
+  puts "I love #{item}"
+end 
+
+# p sports 
+
+# Used .each for hash: 
+
+sports = {"football" => 1, "soccer" => 2, "baseball" => 3, "volleyball" => 4}
+
+sports.each do |key, value|
+  puts "#{key} is my number #{value} sport!"
+end 
+
+# p sports 
+# Used .map! for array:
+
+movie_genres = ['scary', 'drama', 'comedy', 'action']
+
+movie_genres.map! do |genre|
+  puts " I like this #{genre} movie."
+  genre.upcase 
+end 
+
+# p movie_genres
