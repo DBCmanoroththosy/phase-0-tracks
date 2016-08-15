3b99e3c28ab69d4f233c02c93afec7e7df8a3c13
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-14 19:51:01 -0700

    Add case statement to update key and print program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -21,8 +21,30 @@ clients[:decor_theme] = gets.chomp
 puts "Does client like the Cave man basement?"
 clients[:caveman] = gets.chomp 
 
-clients
+#clients
 
 
 # Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
 # Print the latest version of the hash, and exit the program.
+
+case user_input 
+  when 'name'
+	 puts "What's the new info?"
+	 clients[:name] = gets.chomp 
+  when 'age'
+	  puts "What's the new info?"
+	  clients[:age] = gets.chomp 
+  when 'children'
+	puts "What's the new info?"
+	clients[:children] = gets.chomp 
+  when 'decor theme'
+	puts "What's the new info?"
+	clients[:decor_theme] = gets.chomp 
+  when 'cave man'
+	puts "What's the new info?"
+	clients[:caveman] = gets.chomp 
+  else 'none'
+  	puts "Thank you for enter your information!"
+end 
+	
+clients 
