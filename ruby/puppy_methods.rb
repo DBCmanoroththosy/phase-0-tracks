55e7e7ad696f3e2718a326b5c0c9c4a7099e33ac
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-17 12:32:26 -0700

    Add loop

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,34 +1,64 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
-  def speak(num)
-  	puts 'woof' * num 
-  end
+#   def speak(num)
+#   	puts 'woof' * num 
+#   end
 
-  def rollover
-  	puts "*roll over*"
-  end
+#   def rollover
+#   	puts "*roll over*"
+#   end
 
-  def dog_years(age)
-  	puts age * 7
-  end 
+#   def dog_years(age)
+#   	puts age * 7
+#   end 
 
-  def play_dead(length)
-  	puts "plays dead for #{length} per seconds."
-  end
+#   def play_dead(length)
+#   	puts "plays dead for #{length} per seconds."
+#   end
 
-  def initialize 
-  		puts "initializing new puppy instance..."
-  end 
-end
+#   def initialize 
+#   		puts "initializing new puppy instance..."
+#   end 
+# end
 
-spot = Puppy.new 
-spot.fetch('bone')
-spot.speak(3)
-spot.rollover
-spot.dog_years(14)
-spot.play_dead(60)
+# spot = Puppy.new 
+# spot.fetch('bone')
+# spot.speak(3)
+# spot.rollover
+# spot.dog_years(14)
+# spot.play_dead(60)
+
+class Pokemon 
+	def initialize 
+		puts "Adding Pokemon to Pokemon"
+	end 
+
+	def poke_battle(opponent)
+		puts "Go battle #{opponent}!"
+	end 
+
+	def discover(pokemon)
+		puts "You found a wild #{pokemon}"
+	end 
+end 
+
+geodude = Pokemon.new
+geodude.poke_battle("Squirtle")
+geodude.discover("Onyx")
+
+index = 0 
+pokedex = []
+
+while instance < 50 
+	pokemon[index] = pokemon.index
+	pokedex << pokemon 
+	instance += 1 
+end 
+
+
+end 
