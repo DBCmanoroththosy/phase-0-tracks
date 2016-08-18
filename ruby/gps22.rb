1720a0f6d715f9e729642b470cdd243402c9413c
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-18 13:26:24 -0700

    Add initial solution

--- ruby/gps22.rb
+++ ruby/gps22.rb
@@ -8,22 +8,63 @@
   # method to print result and call it here 
 # output: [what data type goes here, array or hash?] 
 
+def create_a_list(incoming_items)
+	grocery_list = {}
+	
+	incoming_items.split(' ').each do |item|
+		grocery_list[item] = 1 
+	end 
+		grocery_list
+end 
+
+my_list = create_a_list("carrots apples cereal pizza")
+
 # Method to add an item to a list
 # input: item name, LIST and optional quantity
 # steps: bring list and add a new item to the list
 # output: update hash list 
+def add_an_item(item, grocery_list)
+	grocery_list[item] = 4
+	grocery_list
+end 
+
+add_an_item("bananas", my_list)
 
 # Method to remove an item from the list
 # input: item name, LIST, quantity
 # steps: delete an item from the list
 # output: update hash list
 
+def remove_an_item(item, grocery_list)
+	grocery_list.delete(item)
+	grocery_list
+end 
+
+remove_an_item("apples", my_list)
+
 # Method to update the quantity of an item
 # input: item name, LIST, quantity
 # steps: change an quantity from the list
 # output: update hash list
 
+def update_quantity(item, grocery_list, quantity)
+	grocery_list[item] = quantity
+	grocery_list
+end 
+
+update_quantity('pizza', my_list, 4)
+
 # Method to print a list and make it look pretty
 # input: add LIST
 # steps: go through this list and print each key with the value
 # output: print hash list 
+
+def print_a_list(grocery_list)
+	puts "Here is my grocery list:"
+	grocery_list.each do |item, quantity|
+		puts "#{item} = #{quantity}"
+	end 
+	
+end 
+
+print_a_list(my_list)
