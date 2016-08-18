107964cb469f12f44839b933f24bc4687e3e73fb
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-17 19:12:19 -0700

    Add pseudocoding

---
+++ ruby/gps22.rb
@@ -0,0 +1,29 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # create an empty hash
+  # bring in the new list and use a method to make it an array
+  # go through array for each value in the array and store it in hash.
+  # set default quantity
+  # method to print result and call it here 
+# output: [what data type goes here, array or hash?] 
+
+# Method to add an item to a list
+# input: item name, LIST and optional quantity
+# steps: bring list and add a new item to the list
+# output: update hash list 
+
+# Method to remove an item from the list
+# input: item name, LIST, quantity
+# steps: delete an item from the list
+# output: update hash list
+
+# Method to update the quantity of an item
+# input: item name, LIST, quantity
+# steps: change an quantity from the list
+# output: update hash list
+
+# Method to print a list and make it look pretty
+# input: add LIST
+# steps: go through this list and print each key with the value
+# output: print hash list 
