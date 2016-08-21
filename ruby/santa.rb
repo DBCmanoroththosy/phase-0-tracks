0728593b1c962ae7310cfa27af9527c3ad02cb6c
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-20 19:13:10 -0700

    Add three attribute changing methods, getter/setter methods and driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -36,27 +36,68 @@ class Santa
 			    ]
 	    @age = 0
 		puts "Initialize Santa Instance..."
+
+		def celebrate_birthday
+			@age = @age + 1
+		end 
+
+		def get_mad_at(reindeer_name)
+			index = 0
+
+			while index < @reindeer_ranking.length 
+				current_index = @reindeer_ranking.index(reindeer_name)
+				if index == current_index
+					@reindeer_ranking.delete_at(current_index)
+				end 
+				index += 1 
+			end 
+			@reindeer_ranking << reindeer_name 
+		end 
+
+		#Add Setter Methods
+		def gender=(new_gender)
+			@gender = new_gender
+		end 
+
+		#Add Getter Methods
+		def age 
+			@age
+		end 
+
+		def ethnicity
+			@ethnicity
+		end 
+		
+		def gender
+			@gender 
+		end 
 	end 
 end 
 
 santa = Santa.new('male','Brazilian')
 santa.speak
 santa.eat_milk_and_cookies("lemon cookie")
+santa.age
+santa.get_mad_at("Vixen")
+santa.gender = "Alien from outer space"
+puts "That Santa told me that he is #{santa.age + 65} years old and a quarter #{santa.ethnicity}."
+puts "He was also a #{santa.gender}."
+p santa
 
 # create an empty array 
 # add some diverse initialization to your file as driver code
 # then loop it
 
-santa = []
-diverse_santas = [['agender','Black'],['female','Latino'],['bigender','White'], ['male','Japanese'],['gender','Hispanic']]
+# santa = []
+# diverse_santas = [['agender','Black'],['female','Latino'],['bigender','White'], ['male','Japanese'],['gender','Hispanic']]
 
-puts "List Of Diverse Santas:"
-diverse_santas.each do |gender, ethnicity|
-	santa << Santa.new(gender, ethnicity)
-	puts "Found a #{ethnicity} #{gender} at the Santa Parade!"
-	puts "Santa instance number #{santa.length} in the array"
-	puts "-" * 25
-end 
+# puts "List Of Diverse Santas:"
+# diverse_santas.each do |gender, ethnicity|
+# 	santa << Santa.new(gender, ethnicity)
+# 	puts "Found a #{ethnicity} #{gender} at the Santa Parade!"
+# 	puts "Santa instance number #{santa.length} in the array"
+# 	puts "-" * 25
+# end 
 
 
 
