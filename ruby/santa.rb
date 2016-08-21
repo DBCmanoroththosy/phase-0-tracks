49b9128c9c6a97c03d894e38645be75406dee1d8
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-20 18:23:20 -0700

    Add release 1 pseudocode and attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,6 +5,12 @@
 # create an eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!" 
 # initialize method 
 
+# Release 1: Attributes
+
+#Pseudocode
+# create a gender and ethnicity attribute, which will be  passes in on initialization
+# enter reindeer_ranking array
+# enter age that has a default of 0 
 class Santa 
 	def speak 
 	  puts "Ho, ho, ho! Haaaapy holidays!"
@@ -14,11 +20,26 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end 
 
-	def initialize
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = [
+			"Rudolph",
+			"Dasher",
+			"Dancer",
+			"Prancer",
+			"Vixen",
+			"Comet",
+			"Cupid",
+			"Donner",
+			"Blitzen"
+			    ]
+	    @age = 0
 		puts "Initialize Santa Instance..."
 	end 
 end 
 
-santa = Santa.new 
+santa = Santa.new('male','Brazilian')
 santa.speak
 santa.eat_milk_and_cookies("lemon cookie")
+
