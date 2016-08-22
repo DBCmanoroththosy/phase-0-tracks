b66dfe9c7d9582a39a6496911156fdb5e01c6da2
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-21 19:46:20 -0700

    Add attr_reader/accessor and fixed initialize method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,16 +3,23 @@
 # create a class 
 # include at least 3 attributes (usgin at least 2 data types)
 # include 3 methods, at least one of which takes an argument
+# release 1: Write your class
+# add attr_reader and attr_accessor
+# write initialize method with at least one attribute to be set 
 
 class Pokedex
-	def initialize(name, tech)
-	@name = name 
-	@tech = tech
-	@location = "Pallet Town"
-	puts "Processing Pokemon election..."
-
+	attr_reader :location, :tech 
+	attr_accessor :name 
+	
+	def initialize(name)
+	  @name = name 
+	  @tech = "flamethrower"
+	  @location = "Pallet Town"
+	  puts "Processing Pokemon selection..."
+	end
+	
 	def speak(say_something)
-	  puts "#{@name} said used #{say_something}"
+	  puts "#{@name} said #{say_something}"
 	end 
 
 	def battle(attack)
@@ -20,16 +27,11 @@ class Pokedex
 	end 
 
 	def fly 
-		puts "#{@name} took off in the air"
+	  puts "#{@name} took off in the air"
 	end 
+end 
 
-
-
-pokemon = Pokedex.new('Charizard', 'Flame toss')
+pokemon = Pokedex.new('Charizard')
 pokemon.speak('Charrrrizzaaardd!')
-pokemon.battle('Body slam')
+pokemon.battle('body slam')
 pokemon.fly
-
-
-
-
