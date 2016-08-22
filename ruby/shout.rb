58662222e42f8fe538a85f9bfd8dc992fa0fbe76
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-22 00:35:51 -0700

    Add mixin version of Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,31 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words + "!!" + " =)"
-  end 
-end
+#   def self.yelling_happily(words)
+#   	words + "!!" + " =)"
+#   end 
+# end
 
-p Shout.yell_angrily("Hola")
-p Shout.yelling_happily("Heyyyyy girlll")
+# p Shout.yell_angrily("Hola")
+# p Shout.yelling_happily("Heyyyyy girlll")
+
+module Shout 
+	def noise(words)
+		"#{words}"
+	end
+end 
+
+class Loud_music
+	include Shout
+end 
+
+class Angry_neighbor
+	include Shout
+end 
+
+music = Loud_music.new
+p music.noise("I like to move it move it...")
+neighbor = Angry_neighbor.new
+p neighbor.noise("Ahhhhh, turn down your music")
