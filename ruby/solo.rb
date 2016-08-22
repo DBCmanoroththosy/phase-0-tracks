20e81b44279ff4230433eba551b2f8c21d4777ed
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-21 19:09:11 -0700

    Add release 0 pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,5 @@
+# pseudocode 
+# release 0: Design a class 
+# create a class 
+# include at least 3 attributes (usgin at least 2 data types)
+# include 3 methods, at least one of which takes an argument
