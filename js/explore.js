b34bcd75a16003ef6b1808e3ffff52f63146a95a
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-23 20:26:32 -0700

    Add psuedocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Release 4: Write a JavaScript Program
+
+// Pseudocode
+// create a function that takes a parameter and reverse the string (i.e. "hello" => "olleh")
+// create a new variable to store the argument's length
+// then create a for loop
+// manipulate the argument string to input the specfic character of the index 
+// put together the string as one 
