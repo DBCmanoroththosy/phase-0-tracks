822c32edc9708abe362f5da3f84de6d3ae815fdb
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-25 11:14:29 -0700

    Add two arrays with additional code that adds another color and name to my arrays

---
+++ js/data_structure.js
@@ -0,0 +1,15 @@
+//Release 0
+//two arrays: colors and names 
+var colors = ['blue','silver','mettalic','gold']
+
+var names = ['Jack', 'Boris', "John", "Jane"]
+
+
+//add code to add name and color 
+colors.push('green')
+
+console.log(colors)
+
+names.push('Trump')
+
+console.log(names)
