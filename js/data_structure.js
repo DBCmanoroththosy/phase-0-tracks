0e554168e6e7560251e81823ec2eca50ad7beb9c
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-25 11:21:21 -0700

    Add release 1: created a loop and creates our hash

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,15 +1,18 @@
-//Release 0
-//two arrays: colors and names 
-var colors = ['blue','silver','mettalic','gold']
-
-var names = ['Jack', 'Boris', "John", "Jane"]
-
-
-//add code to add name and color 
-colors.push('green')
-
-console.log(colors)
-
-names.push('Trump')
-
-console.log(names)
+// 7.2 Christopher Mendoza & Manoroth Thosy
+// Release 0
+// Create two arrays: Colors and Names
+var colors = ['blue', 'silver', 'mettalic', 'gold'];
+var names = ['jack', 'boris', 'john', 'jane'];
+// add code to add name and colors
+colors.push('green');
+console.log(colors);
+names.push('trump');
+console.log(names);
+//Release 1
+// initiate an empty hash
+var horses = {};
+// create a loop that matches index position of names as keys to colors and values.
+for ( var i = 0; i < names.length; i++ ) {
+  horses[names[i]] = colors[i];
+}
+console.log(horses);
