5fae3ee4f277742489cc4e530e5af92227741a3a
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-29 02:08:12 -0700

    Add reflection

--- gps6/my_solution.rb
+++ gps6/my_solution.rb
@@ -91,3 +91,13 @@ end
 
 #=======================================================================
 # Reflection Section
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# You can see there is a first hash that accesses all 50 states and another one that accesses the key value pairs for each state.
+
+# What does require_relative do? How is it different from require?
+# Require_relative is a cool way to link any file to another file, so we can exchange information or access them.
+# Require_relative complements the builtin method require by allowing you to load a file that is relative to the file containing the require_relative statement.
+# When you use require to load a file, you are usually accessing functionality that has been properly installed, and made accessible, in your system. require does not offer a good solution for loading files within the project’s code. This may be useful during a development phase, for accessing test data, or even for accessing files that are "locked" away inside a project, not intended for outside use.
+
+# What are some ways to iterate through a hash?
+# You can use many different built-in methods (ie. .each and .map )
